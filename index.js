3dc849883c3377b911213caed9842ba50a8deeaa
ahmed youssef elsayed <ahmedyoussef59@yahoo.com>
2018-03-29 10:41:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function multiply(a, b) { return a*b;}
 function divide(a, b) { return a/b;}
 function inc(a) { return a + 1;}
 function dec(a) { return a - 1;}
-function makeInt(a) { if (a.string()= true) { return a;}
+function makeInt(a) { if (a.string()) { return a;}}
+
   
 
   
